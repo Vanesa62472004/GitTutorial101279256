192af067e13a6e95f74e7980fb89ca364c7730b9
Vanesa62472004 <anadeleonbonilla@cmail.carleton.ca>
2024-09-28 11:49:05 -0400

    Add files via upload

---
+++ File.cc
@@ -0,0 +1,35 @@
+#include "File.h"
+
+using namespace std;
+
+// File(const string& inName, const string& inContent, const Date& date);
+
+File::File(const string& inName, const string& inContent, const Date& date){}
+
+Date& File::getDate( ){
+    return date;
+}
+
+string File::getContent ( ){
+    return content;
+}
+
+string File::getName ( ){
+    return name;
+}
+
+
+bool File::lessThan(const File& other_date ){
+    return date.lessThan(other_date);
+}
+
+void File::print( ) {
+    cout << "File: " << name << endl;
+    cout << "\nLast touched: " << endl;
+    date.print( );
+}
+
+void File::printContents( ) {
+    print( );
+    cout << "Content: \n" << content << endl;
+}
---
+++ File.h
@@ -0,0 +1,40 @@
+// header guard
+#ifndef __FILE_H__
+#define __FILE_H__
+
+#include <iostream>
+#include <string>
+
+#include "Date.h"
+
+using namespace std;
+
+class File {
+    public:
+        // Default constructor, not applicable
+        
+        // Constructor with arguments
+        File(const string& inName, const string& inContent, const Date& date);
+        
+        // getters
+        Date& date( );
+        string getName ( );
+        string getContent ( );
+
+        // helpers
+        bool lessThan(const File& other_date);
+        
+        // print function
+        void print( );
+        void printContents( );
+
+
+    private:
+        string name;
+        string content;
+        Date date;        
+
+};
+
+
+#endif /* __FILE_H__ */
